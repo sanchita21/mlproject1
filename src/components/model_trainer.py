da0e44582d33066c00838c8d248e97ab5793ec15
Sanchita Deb <sanchitadeb21@gmail.com>
2023-06-15 23:23:42 +0530

    hyper parameter train

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,51 +41,80 @@ class ModelTrainer:
 
             models={
                 "Linear Regression": LinearRegression(),
-                "Lasso": Lasso(),
-                "Ridge": Ridge(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Random Forest Regressor": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "XGBRegressor": XGBRegressor(), 
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test,
-                                             models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                 
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
             
+             
+            model_report:dict=evaluate_models(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test,
+                                                models=models,param=params)
+
 
             # to get the best model score from dict
             best_model_score=max(sorted(model_report.values()))
 
-             # to get the best model name from dict
+                # to get the best model name from dict
             best_model_name = max(model_report, key=lambda x: model_report[x])
-            
+
             ## [list(model_report.values()).index(best_model_score)][0]
-            ##earlier code best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)][0]
+            ##earlier code best_model_name = list(model_report.keys()) 
+            #[list(model_report.values()).index(best_model_score)][0]
 
             best_model = models[best_model_name]
-        
+
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             logging.info(f"best model on both training and test dataset")
-            
+
             save_object(
-                         file_path=self.model_trainer_config.trained_model_file_path,
-                         obj=best_model
-                              )
+            file_path=self.model_trainer_config.trained_model_file_path,
+                            obj=best_model
+                                )
 
             predicted=best_model.predict(X_test)
             r2_square=r2_score(y_test, predicted)
+
             return r2_square
-
-
         except Exception as e:
             raise CustomException(e,sys) 
-
-
-
-
-
-
-
